e4f5dfe41377f8582f3935eb592504879d0d5724
TaeSeok <sukyuree@naver.com>
2022-09-01 14:47:20 +0900

    개발환경 세팅 시작

---
+++ src/main/java/com/example/MPBE/MpbeApplication.java
@@ -0,0 +1,13 @@
+package com.example.MPBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpbeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MPBE/MpbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MPBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
