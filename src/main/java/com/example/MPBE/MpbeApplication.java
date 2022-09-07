27a95441c866b0970daa0fefbf4a0277ff9cf3f5
TaeSeok <sukyuree@naver.com>
2022-09-07 20:59:57 +0900

    jwt Token, Spring Security, Jasypt 세팅

--- src/main/java/com/example/MPBE/MpbeApplication.java
+++ src/main/java/com/example/MPBE/MpbeApplication.java
@@ -1,12 +1,16 @@
 package com.example.MPBE;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MpbeApplication {
+	@Value("${jasypt.encryptor.password}")
+	private static String PASSWORD;
 
 	public static void main(String[] args) {
+		System.out.println(PASSWORD);
 		SpringApplication.run(MpbeApplication.class, args);
 	}
 
--- src/test/java/com/example/MPBE/MpbeApplicationTests.java
+++ src/test/java/com/example/MPBE/MpbeApplicationTests.java
@@ -1,6 +1,8 @@
 package com.example.MPBE;
 
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -8,6 +10,20 @@ class MpbeApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void jasypt_encrypt_decrypt_test() {
+		String plainText = "plainText";
+
+		StandardPBEStringEncryptor jasypt = new StandardPBEStringEncryptor();
+		jasypt.setPassword("password");
+
+		String encryptedText = jasypt.encrypt(plainText);
+		String decryptedText = jasypt.decrypt(encryptedText);
+
+		System.out.println(encryptedText);
+		System.out.println(decryptedText);
 	}
 
 }
