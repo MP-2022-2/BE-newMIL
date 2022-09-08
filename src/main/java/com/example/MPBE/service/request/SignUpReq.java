5ad65c981b1c2ba855d4bb8cc2ceb18ad0329cab
TaeSeok <sukyuree@naver.com>
2022-09-08 19:26:47 +0900

    SignUp Request 구현

--- src/main/java/com/example/MPBE/service/request/SignUpReq.java
+++ src/main/java/com/example/MPBE/service/request/SignUpReq.java
@@ -1,12 +1,46 @@
 package com.example.MPBE.service.request;
 
+import com.example.MPBE.domain.model.Identity;
+import com.example.MPBE.domain.model.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import javax.validation.constraints.NotNull;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class SignUpReq {
+    @NotNull
+    String userId;
 
+    @NotNull
+    String name;
+
+    @NotNull
+    String email;
+
+    @NotNull
+    String password;
+
+    @NotNull
+    Integer studentId;
+
+    @NotNull
+    Identity identity;
+
+    String company;
+
+    public User toUserModel() {
+        return User.builder()
+                .name(name)
+                .email(email)
+                .password(password)
+                .identity(identity)
+                .studentId(studentId)
+                .userId(userId)
+                .company(company)
+                .build();
+    }
 }
