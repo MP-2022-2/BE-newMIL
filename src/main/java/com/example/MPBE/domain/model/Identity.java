4b48c3a9f654f1c1677ebbc007f1ccfb1a9ce62c
TaeSeok <sukyuree@naver.com>
2022-09-08 19:18:57 +0900

    도메인 레이어 유저 부분 구축

---
+++ src/main/java/com/example/MPBE/domain/model/Identity.java
@@ -0,0 +1,5 @@
+package com.example.MPBE.domain.model;
+
+public enum Identity {
+    ADMIN, STUDENT, GRADUATE
+}
--- src/main/java/com/example/MPBE/domain/model/User.java
+++ src/main/java/com/example/MPBE/domain/model/User.java
@@ -1,4 +1,35 @@
 package com.example.MPBE.domain.model;
 
-public class User {
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User extends BaseModel{
+
+    @NotNull
+    String userId;
+
+    @NotNull
+    String name;
+
+    @NotNull
+    String email;
+
+    @NotNull
+    String password;
+
+    @NotNull
+    Integer studentId;
+
+    @NotNull
+    Identity identity;
+
+    String company;
 }
