cd928491af00a5a709b1194da5e151872321da9b
sukyuree <sukyuree@naver.com>
2022-09-18 17:58:53 +0900

    AWS 세팅

--- src/test/java/com/example/MPBE/MpbeApplicationTests.java
+++ src/test/java/com/example/MPBE/MpbeApplicationTests.java
@@ -8,22 +8,4 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MpbeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
-	@Test
-	public void jasypt_encrypt_decrypt_test() {
-		String plainText = "plainText";
-
-		StandardPBEStringEncryptor jasypt = new StandardPBEStringEncryptor();
-		jasypt.setPassword("password");
-
-		String encryptedText = jasypt.encrypt(plainText);
-		String decryptedText = jasypt.decrypt(encryptedText);
-
-		System.out.println(encryptedText);
-		System.out.println(decryptedText);
-	}
-
 }
