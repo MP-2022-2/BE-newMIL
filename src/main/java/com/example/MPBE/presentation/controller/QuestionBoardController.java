ded7f51f162f3b766f57ad2f42a5e4f2a68b7049
sukyuree <sukyuree@naver.com>
2022-12-08 22:04:58 +0900

    hot fix

--- src/main/java/com/example/MPBE/presentation/controller/QuestionBoardController.java
+++ src/main/java/com/example/MPBE/presentation/controller/QuestionBoardController.java
@@ -4,7 +4,7 @@ import com.example.MPBE.service.dto.PostDto;
 import com.example.MPBE.service.request.CommentReq;
 import com.example.MPBE.service.request.PostReq;
 import com.example.MPBE.service.response.BaseResponse;
-import com.example.MPBE.service.response.PostListReq;
+import com.example.MPBE.service.response.PostListRes;
 import com.example.MPBE.service.response.PostRes;
 import com.example.MPBE.service.service.BoardService;
 import lombok.RequiredArgsConstructor;
@@ -28,7 +28,7 @@ public class QuestionBoardController {
 
     @GetMapping("/question")
     public ResponseEntity<? extends BaseResponse> getQuestionBoardPosts(@Valid Pageable pageable){
-        return ResponseEntity.status(200).body(new PostListReq("글 목록 조회 완료",200,boardService.getQuestionBoardAll(pageable)));
+        return ResponseEntity.status(200).body(new PostListRes("글 목록 조회 완료",200,boardService.getQuestionBoardAll(pageable)));
     }
 
     @GetMapping("/question/{id}")
