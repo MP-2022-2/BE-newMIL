e1bfc5700d95148ce618dffc3317fdeddf576a62
sukyuree <sukyuree@naver.com>
2022-12-09 17:52:24 +0900

    super hot fix2

--- src/main/java/com/example/MPBE/service/dto/CommentDto.java
+++ src/main/java/com/example/MPBE/service/dto/CommentDto.java
@@ -1,6 +1,7 @@
 package com.example.MPBE.service.dto;
 
 import com.example.MPBE.domain.model.Comment;
+import com.example.MPBE.util.enums.BoardType;
 import com.sun.istack.NotNull;
 import lombok.Getter;
 
@@ -20,8 +21,12 @@ public class CommentDto {
     @NotNull
     Long postId;
 
+    @NotNull
+    BoardType boardType;
+
     public CommentDto(Comment comment){
         this.text = comment.getText();
+        this.boardType = comment.getPost().getBoardType();
         this.createdAt = comment.getCreatedAt();
         this.nickName = comment.getUser().getNickName();
         this.postId = comment.getPost().getId();
